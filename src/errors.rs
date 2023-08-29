efd31ccc1cf25b2662c71e4772146a8ea26ba786
Denis Selimovic <denis.selimovic@klika.ba>
2023-08-29 22:10:04 +0200

    Add new parser error type

--- src/errors.rs
+++ src/errors.rs
@@ -6,3 +6,13 @@ pub enum LexError {
     #[error("string not terminated")]
     NotTerminated,
 }
+
+#[derive(Debug, Error)]
+pub enum ParserError {
+    #[error("Error during lexical analysis: {0}")]
+    Lex(String),
+    #[error("Unexpected token. Expected {0} got {1}")]
+    UnexpectedToken(String, String),
+    #[error("Expected token, but reached end of file")]
+    EndOfFile,
+}
