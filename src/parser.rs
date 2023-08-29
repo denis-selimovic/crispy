5b8fb85ce6ae10ed12a570d768ec9d53a698b719
Denis Selimovic <denis.selimovic@klika.ba>
2023-08-29 22:27:52 +0200

    Add parser unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -3,14 +3,18 @@ use crate::errors::ParserError;
 use crate::lexer::{lex, Lexem};
 use std::rc::Rc;
 
-pub fn parser(input: &str) -> Result<ASTNode, ParserError> {
+pub fn parse(input: &str) -> Result<ASTNode, ParserError> {
     let lex_analysis = lex(input);
 
     if let Err(err) = lex_analysis {
         return Err(ParserError::Lex(err.to_string()));
     }
 
-    let mut lexems = lex_analysis.unwrap_or_default();
+    let mut lexems = lex_analysis
+        .unwrap_or_default()
+        .into_iter()
+        .rev()
+        .collect::<Vec<_>>();
     let parsed = parse_from_lexems(&mut lexems)?;
 
     Ok(parsed)
@@ -55,4 +59,61 @@ fn parse_from_lexems(lexems: &mut Vec<Lexem>) -> Result<ASTNode, ParserError> {
     }
 
     Ok(ASTNode::List(Rc::new(data)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiply() {
+        let prog = "(/ 3 2)";
+        let ast = parse(prog).unwrap();
+
+        assert_eq!(
+            ast,
+            ASTNode::List(Rc::new(vec![
+                ASTNode::BinaryOp("/".to_string()),
+                ASTNode::Int(3),
+                ASTNode::Int(2),
+            ]))
+        )
+    }
+
+    #[test]
+    fn test_calculate_area() {
+        let prog = "
+            (
+                (define r 2)
+                (define pi 3.14)
+                (* pi (* r r))
+            )
+        ";
+        let ast = parse(prog).unwrap();
+
+        assert_eq!(
+            ast,
+            ASTNode::List(Rc::new(vec![
+                ASTNode::List(Rc::new(vec![
+                    ASTNode::Keyword("define".to_string()),
+                    ASTNode::Symbol("r".to_string()),
+                    ASTNode::Int(2),
+                ])),
+                ASTNode::List(Rc::new(vec![
+                    ASTNode::Keyword("define".to_string()),
+                    ASTNode::Symbol("pi".to_string()),
+                    ASTNode::Float(3.14),
+                ])),
+                ASTNode::List(Rc::new(vec![
+                    ASTNode::BinaryOp("*".to_string()),
+                    ASTNode::Symbol("pi".to_string()),
+                    ASTNode::List(Rc::new(vec![
+                        ASTNode::BinaryOp("*".to_string()),
+                        ASTNode::Symbol("r".to_string()),
+                        ASTNode::Symbol("r".to_string()),
+                    ])),
+                ]))
+            ]))
+        )
+    }
 }
