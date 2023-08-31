8551dae7f986ad3d428ac44d72f93259d3ff9866
Denis Selimovic <denis.selimovic@klika.ba>
2023-08-31 20:30:22 +0200

    Finish main pub declarations for lib

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-use std::{error, fmt::Debug};
+use std::fmt::Debug;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ mod exec;
 mod lexer;
 mod parser;
 mod scope;
+
+pub use ast::ASTNode;
+pub use errors::CompileError;
+pub use exec::exec;
+pub use scope::Scope;
